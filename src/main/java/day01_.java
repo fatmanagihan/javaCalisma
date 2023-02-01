e43e98dca68f4dd95bc8d9bb8f22c131ff3ab759
Emin Çankaya <emincankaya30@gmail.com>
2023-02-01 17:27:57 +0300

    ilk degisiklik

--- src/main/java/day01_.java
+++ src/main/java/day01_.java
@@ -2,5 +2,7 @@ public class day01_ {
 
     public static void main(String[] args) {
         System.out.println("bu bir deneme baslangicidir");
+
+        System.out.println("javadaki calismalarimi buradan yapacagim");
     }
 }
